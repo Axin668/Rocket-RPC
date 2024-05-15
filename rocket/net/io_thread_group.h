0b93acadc5b13fbc49161ee1f8ca14edf31e0c26
Axin668 <axin99966@163.com>
2024-05-15 22:34:41 +0800

    feat: 完成IO线程组(IO Thread Group)模块

---
+++ rocket/net/io_thread_group.h
@@ -0,0 +1,33 @@
+#ifndef ROCKET_RPC_NET_IO_THREAD_GROUP_H
+#define ROCKET_RPC_NET_IO_THREAD_GROUP_H
+
+#include <vector>
+#include <rocket/common/log.h>
+#include <rocket/net/io_thread.h>
+
+namespace rocket_rpc {
+
+class IOThreadGroup {
+
+  public:
+    IOThreadGroup(int size);
+
+    ~IOThreadGroup();
+
+    void start();
+
+    void join();
+
+    IOThread* getIOThread();
+
+  private:
+
+    int m_size {0};
+    std::vector<IOThread*> m_io_thread_groups;
+
+    int m_index {0};
+};
+
+}
+
+#endif
--- testcases/test_eventloop.cc
+++ testcases/test_eventloop.cc
@@ -4,18 +4,16 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <memory>
 #include "rocket/common/log.h"
 #include "rocket/common/config.h"
 #include "rocket/net/fd_event.h"
 #include "rocket/net/eventloop.h"
-#include "rocket/net/timer.h"
+#include "rocket/net/timer_event.h"
+#include "rocket/net/io_thread.h"
+#include "rocket/net/io_thread_group.h"
 
-int main() {
-
-  rocket_rpc::Config::SetGlobalConfig("../conf/rocket_rpc.xml");
-  rocket_rpc::Logger::InitGlobalLogger();
-
-  rocket_rpc::EventLoop* eventloop = new rocket_rpc::EventLoop();
+void test_io_thread() {
 
   int listenfd = socket(AF_INET, SOCK_STREAM, 0);
   if (listenfd == -1) {
@@ -52,8 +50,6 @@ int main() {
     DEBUGLOG("success get client fd[%d], peer addr: [%s:%d]", clientfd, inet_ntoa(peer_addr.sin_addr), ntohs(peer_addr.sin_port));
   });
 
-  eventloop->addEpollEvent(&event);
-
   int i = 0;
   rocket_rpc::TimerEvent::s_ptr timer_event = std::make_shared<rocket_rpc::TimerEvent>(
     1000, true, [&i]() {
@@ -61,8 +57,81 @@ int main() {
     }
   );
 
-  eventloop->addTimerEvent(timer_event);
-  eventloop->loop();
+  // rocket_rpc::IOThread io_thread;
+
+  // io_thread.getEventLoop()->addEpollEvent(&event);
+  // io_thread.getEventLoop()->addTimerEvent(timer_event);
+  // io_thread.start();
+
+  // io_thread.join();
+
+  rocket_rpc::IOThreadGroup io_thread_group(2);
+  rocket_rpc::IOThread* io_thread = io_thread_group.getIOThread();
+  io_thread->getEventLoop()->addEpollEvent(&event);
+  io_thread->getEventLoop()->addTimerEvent(timer_event);
+
+  rocket_rpc::IOThread* io_thread2 = io_thread_group.getIOThread();
+  io_thread2->getEventLoop()->addTimerEvent(timer_event);
+
+  io_thread_group.start();
+  io_thread_group.join();
+}
+
+int main() {
+
+  rocket_rpc::Config::SetGlobalConfig("../conf/rocket_rpc.xml");
+  rocket_rpc::Logger::InitGlobalLogger();
+
+  test_io_thread();
+
+  // rocket_rpc::EventLoop* eventloop = new rocket_rpc::EventLoop();
+
+  // int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  // if (listenfd == -1) {
+  //   ERRORLOG("listenfd = -1");
+  //   exit(0);
+  // }
+
+  // sockaddr_in addr;
+  // memset(&addr, 0, sizeof(addr));
+
+  // addr.sin_port = htons(12345);
+  // addr.sin_family = AF_INET;
+  // inet_aton("127.0.0.1", &addr.sin_addr);
+
+  // int rt = bind(listenfd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+  // if (rt != 0) {
+  //   ERRORLOG("bind error");
+  //   exit(1);
+  // }
+
+  // rt = listen(listenfd, 100);
+  // if (rt != 0) {
+  //   ERRORLOG("listen error");
+  //   exit(1);
+  // }
+
+  // rocket_rpc::FdEvent event(listenfd);
+  // event.listen(rocket_rpc::FdEvent::IN_EVENT, [listenfd](){
+  //   sockaddr_in peer_addr;
+  //   socklen_t addr_len = sizeof(peer_addr);
+  //   memset(&peer_addr, 0, sizeof(peer_addr));
+  //   int clientfd = accept(listenfd, reinterpret_cast<sockaddr*>(&peer_addr), &addr_len);
+
+  //   DEBUGLOG("success get client fd[%d], peer addr: [%s:%d]", clientfd, inet_ntoa(peer_addr.sin_addr), ntohs(peer_addr.sin_port));
+  // });
+
+  // eventloop->addEpollEvent(&event);
+
+  // int i = 0;
+  // rocket_rpc::TimerEvent::s_ptr timer_event = std::make_shared<rocket_rpc::TimerEvent>(
+  //   1000, true, [&i]() {
+  //     INFOLOG("trigger timer event, count=%d", i++);
+  //   }
+  // );
+
+  // eventloop->addTimerEvent(timer_event);
+  // eventloop->loop();
 
   return 0;
 }
