40308aed52adf5c76b608d68f2dae01598fb2812
Axin668 <axin99966@163.com>
2024-05-16 22:54:09 +0800

    feat: 完成 tcp acceptor 模块

---
+++ rocket/net/tcp/tcp_acceptor.h
@@ -0,0 +1,26 @@
+#ifndef ROCKET_RPC_NET_TCP_TCP_ACCEPTOR_H
+#define ROCKET_RPC_NET_TCP_TCP_ACCEPTOR_H
+
+#include "rocket/net/tcp/net_addr.h"
+
+namespace rocket_rpc {
+
+class TcpAcceptor {
+  public:
+    TcpAcceptor(NetAddr::s_ptr local_addr);
+
+    ~TcpAcceptor();
+
+    int accept();
+
+  private:
+    NetAddr::s_ptr m_local_addr; // 服务端监听的地址, addr -> ip:port
+
+    int m_family {-1};
+
+    int m_listenfd {-1}; // 监听套接字
+};
+
+}
+
+#endif
